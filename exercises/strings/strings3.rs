c259f3c85b797edb9a45224b5e3db0ae72b04f86
SSSSiren <1345487300@qq.com>
2025-04-09 16:17:37 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,29 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut tar = input.to_string();
+    tar.push_str(" world!");
+    tar
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let offsite = input.find("cars").unwrap_or(input.len());
+
+    let cars_len = "cars".len();
+    let mut tar = input.to_string();
+    tar.replace_range(offsite..offsite+cars_len, "balloons");
+    tar;
+    // 直接使用replace 更快
+    input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
